285123e99a3031b0de387819c13e52acc2037ff9
Neibue <devwithmax@outlook.com>
2020-10-27 11:36:11 +0100

    Adding set model

---
+++ src/models/set.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const setSchema = new mongoose.Schema({
+    name: String,
+    imageUrl: String,
+    encyclopediaUrl: String,
+    level: Number,
+    bonus: Array,
+    recipe: Array({
+        name: String,
+        imageUrl: String,
+        amount: Number
+    })
+});
+
+const Set = mongoose.model('Set', setSchema);
+
+module.exports = Set;
