c76abbea23feed4ea5e3394b2e17c15800f4b5d8
Neibue <devwithmax@outlook.com>
2020-10-28 17:14:35 +0100

    Modify to implement the equipment component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Equipments />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Equipments from './components/Equipments.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Equipments
   }
 }
 </script>
@@ -24,5 +23,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 80%;
+  margin: auto;
 }
 </style>
